b0b230d5ff8ba50919ae7d47dbc626896018bdff
chenkongliang <chenkongliang@haochengji100.com>
2021-08-13 23:06:13 +0800

    添加kt 	modified:   www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-extensions.kt

--- www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-extensions.kt
+++ www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-extensions.kt
@@ -139,6 +139,132 @@ fun usage(baz: Baz) {
 }
  */
 
+/*
+扩展声明为成员
+在一个类内部你可以为另一个类声明扩展。
+在这个扩展中，有个多个隐含的接受者，其中扩展方法定义所在类的实例称为分发接受者，
+而扩展方法的目标类型的实例称为扩展接受者。
+
+在 F 类内，创建了 G 类的扩展。此时，F 被成为分发接受者，
+而 G 为扩展接受者。从上例中，可以清楚的看到，在扩展函数中，可以调用派发接收者的成员函数。
+ */
+
+class G {
+    fun bar() = println("G.bar()")
+}
+
+class F {
+    fun baz() = println("F.baz()")
+    fun bar() = println("F.bar()")
+
+    fun G.foo() {
+        bar() //调用G.bar,扩展接受者优先
+        baz() //调用F.baz
+        this@F.bar() //调用F.bar
+        //假如在调用某一个函数，而该函数在分发接受者和扩展接受者均存在，
+        //则以扩展接收者优先，要引用分发接收者的成员你可以使用限定的 this 语法。
+    }
+
+    fun caller(g:G) {
+        g.foo() //调用扩展函数
+    }
+}
+
+fun testFG() {
+    println("testFG")
+    val f = F()
+    val g = G()
+    f.caller(g)
+    //输出：
+    //G.bar()
+    //F.baz()
+}
+
+/*
+以成员的形式定义的扩展函数, 可以声明为 open , 而且可以在子类中覆盖.
+也就是说, 在这类扩展函数的派 发过程中, 针对分发接受者是虚拟的(virtual),
+ 但针对扩展接受者仍然是静态的。
+ */
+
+open class M {}
+class M1 : M() {}
+open class N {
+    open fun M.foo() = println("M.foo in N")
+    open fun M1.foo() = println("M1.foo in N")
+    fun caller(m:M) {
+        m.foo()//调用扩展函数
+    }
+}
+
+class N1 : N() {
+    override fun M.foo() = println("M.foo in N1")
+    override fun M1.foo() = println("M1.foo in N1")
+}
+
+fun testMN() {
+    println("testMN")
+    N().caller(M())   //M.foo in N
+    N1().caller(M())  //M.foo in N1  —— 分发接收者虚拟解析
+    N().caller(M1())  //M.foo in N   —— 扩展接收者静态解析
+    N1().caller(M1()) //M.foo in N1
+}
+/*
+伴生对象内的成员相当于 Java 中的静态成员，其生命周期伴随类始终，在伴生对象内部可以定义变量和函数，这些变量和函数可以直接用类名引用。
+
+对于伴生对象扩展函数，有两种形式，一种是在类内扩展，一种是在类外扩展，这两种形式扩展后的函数互不影响（甚至名称都可以相同），即使名称相同，它们也完全是两个不同的函数，并且有以下特点：
+
+ （1）类内扩展的伴随对象函数和类外扩展的伴随对象可以同名，它们是两个独立的函数，互不影响；
+ （2）当类内扩展的伴随对象函数和类外扩展的伴随对象同名时，类内的其它函数优先引用类内扩展的伴随对象函数，即对于类内其它成员函数来说，类内扩展屏蔽类外扩展；
+ （3）类内扩展的伴随对象函数只能被类内的函数引用，不能被类外的函数和伴随对象内的函数引用；
+ （4）类外扩展的伴随对象函数可以被伴随对象内的函数引用，；
+例如以下代码：
+
+class MyClass {
+    companion object {
+        val myClassField1: Int = 1
+        var myClassField2 = "this is myClassField2"
+        fun companionFun1() {
+            println("this is 1st companion function.")
+            foo()
+        }
+        fun companionFun2() {
+            println("this is 2st companion function.")
+            companionFun1()
+        }
+    }
+    fun MyClass.Companion.foo() {
+        println("伴随对象的扩展函数（内部）")
+    }
+    fun test2() {
+        MyClass.foo()
+    }
+    init {
+        test2()
+    }
+}
+val MyClass.Companion.no: Int
+    get() = 10
+fun MyClass.Companion.foo() {
+    println("foo 伴随对象外部扩展函数")
+}
+fun main(args: Array<String>) {
+    println("no:${MyClass.no}")
+    println("field1:${MyClass.myClassField1}")
+    println("field2:${MyClass.myClassField2}")
+    MyClass.foo()
+    MyClass.companionFun2()
+}
+运行结果：
+
+no:10
+field1:1
+field2:this is myClassField2
+foo 伴随对象外部扩展函数
+this is 2st companion function.
+this is 1st companion function.
+没有验证????foo 伴随对象外部扩展函数????
+ */
+
 fun main(args: Array<String>) {
     testUser()
     testSwap()
@@ -150,4 +276,6 @@ fun main(args: Array<String>) {
     println(t.toString())
 
     testCompanionObject()
+    testFG()
+    testMN()
 }
