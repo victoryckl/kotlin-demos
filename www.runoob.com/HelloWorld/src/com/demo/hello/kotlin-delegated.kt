1f7fd77cc4aa9c0570e8944d8925b69145862e5b
chenkongliang <chenkongliang@haochengji100.com>
2021-08-26 13:47:08 +0800

    www.runoob.com/kotlin 学习完成 	modified:   www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-delegated.kt

--- www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-delegated.kt
+++ www.runoob.com/HelloWorld/src/com/demo/hello/kotlin-delegated.kt
@@ -1,5 +1,6 @@
 package com.demo.hello
 
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 /*
@@ -95,6 +96,161 @@ val lazyValue : String by lazy {
 //    "Hello"
 //})
 
+/*
+可观察属性 Observable
+observable 可以用于实现观察者模式。
+
+Delegates.observable() 函数接受两个参数: 第一个是初始化值,
+第二个是属性值变化事件的响应器(handler)。
+在属性赋值后会执行事件的响应器(handler)，它有三个参数：被赋值的属性、旧值和新值：
+ */
+class User3 {
+    var name : String by Delegates.observable("初始值") {
+        property, oldValue, newValue ->  println("$property 旧值：$oldValue -> 新值：$newValue")
+    }
+}
+
+/*
+把属性储存在映射中
+一个常见的用例是在一个映射（map）里存储属性的值。
+这经常出现在像解析 JSON 或者做其他"动态"事情的应用中。
+在这种情况下，你可以使用映射实例自身作为委托来实现委托属性。
+ */
+class Site4(map : Map<String, Any?>) {
+    val name : String by map // var name -> Error:(120, 26) Kotlin: Missing 'setValue(Site4, KProperty<*>, String)' method on delegate of type 'Map<String, Any?>'
+    val url  : String by map
+}
+
+//如果使用 var 属性，需要把 Map 换成 MutableMap：
+class Site5(map : MutableMap<String, Any?>) {
+    var name : String by map
+    val url  : String by map
+}
+
+//Not Null
+//notNull 适用于那些无法在初始化阶段就确定属性值的场合。
+//需要注意，如果属性在赋值前就被访问的话则会抛出异常。
+class Foo2 {
+    var notNullBar : String by Delegates.notNull<String>()
+}
+
+/*
+局部委托属性
+你可以将局部变量声明为委托属性。 例如，你可以使一个局部变量惰性初始化：
+fun example(computeFoo: () -> Foo) {
+    val memoizedFoo by lazy(computeFoo)
+
+    if (someCondition && memoizedFoo.isValid()) {
+        memoizedFoo.doSomething()
+    }
+}
+memoizedFoo 变量只会在第一次访问时计算。 如果 someCondition 失败，那么该变量根本不会计算。
+*/
+
+/*
+属性委托要求
+对于只读属性(也就是说val属性), 它的委托必须提供一个名为getValue()的函数。该函数接受以下参数：
+
+thisRef —— 必须与属性所有者类型（对于扩展属性——指被扩展的类型）相同或者是它的超类型
+property —— 必须是类型 KProperty<*> 或其超类型
+这个函数必须返回与属性相同的类型（或其子类型）。
+
+对于一个值可变(mutable)属性(也就是说,var 属性),除 getValue()函数之外,
+它的委托还必须 另外再提供一个名为setValue()的函数, 这个函数接受以下参数:
+
+property —— 必须是类型 KProperty<*> 或其超类型
+new value —— 必须和属性同类型或者是它的超类型。
+ */
+
+/*
+翻译规则
+在每个委托属性的实现的背后，Kotlin 编译器都会生成辅助属性并委托给它。
+例如，对于属性 prop，生成隐藏属性 prop$delegate，而访问器的代码只是简单地委托给这个附加属性：
+
+class C {
+    var prop: Type by MyDelegate()
+}
+
+// 这段是由编译器生成的相应代码：
+class C {
+    private val prop$delegate = MyDelegate()
+    var prop: Type
+        get() = prop$delegate.getValue(this, this::prop)
+        set(value: Type) = prop$delegate.setValue(this, this::prop, value)
+}
+Kotlin 编译器在参数中提供了关于 prop 的所有必要信息：
+第一个参数 this 引用到外部类 C 的实例，
+而 this::prop 是 KProperty 类型的反射对象，该对象描述 prop 自身。
+ */
+
+
+/*
+提供委托
+通过定义 provideDelegate 操作符，可以扩展创建属性实现所委托对象的逻辑。
+如果 by 右侧所使用的对象将 provideDelegate 定义为成员或扩展函数，
+那么会调用该函数来 创建属性委托实例。
+
+provideDelegate 的一个可能的使用场景是在创建属性时
+（而不仅在其 getter 或 setter 中）检查属性一致性。
+
+例如，如果要在绑定之前检查属性名称，可以这样写：
+class ResourceLoader<T>(id: ResourceID<T>) {
+    operator fun provideDelegate(
+            thisRef: MyUI,
+            prop: KProperty<*>
+    ): ReadOnlyProperty<MyUI, T> {
+        checkProperty(thisRef, prop.name)
+        // 创建委托
+    }
+
+    private fun checkProperty(thisRef: MyUI, name: String) { …… }
+}
+
+fun <T> bindResource(id: ResourceID<T>): ResourceLoader<T> { …… }
+
+class MyUI {
+    val image by bindResource(ResourceID.image_id)
+    val text by bindResource(ResourceID.text_id)
+}
+provideDelegate 的参数与 getValue 相同：
+
+thisRef —— 必须与 属性所有者 类型（对于扩展属性——指被扩展的类型）相同或者是它的超类型
+property —— 必须是类型 KProperty<*> 或其超类型。
+在创建 MyUI 实例期间，为每个属性调用 provideDelegate 方法，并立即执行必要的验证。
+
+如果没有这种拦截属性与其委托之间的绑定的能力，为了实现相同的功能， 你必须显式传递属性名，这不是很方便：
+
+// 检查属性名称而不使用“provideDelegate”功能
+class MyUI {
+    val image by bindResource(ResourceID.image_id, "image")
+    val text by bindResource(ResourceID.text_id, "text")
+}
+
+fun <T> MyUI.bindResource(
+        id: ResourceID<T>,
+        propertyName: String
+): ReadOnlyProperty<MyUI, T> {
+   checkProperty(this, propertyName)
+   // 创建委托
+}
+在生成的代码中，会调用 provideDelegate 方法来初始化辅助的 prop$delegate 属性。
+比较对于属性声明 val prop: Type by MyDelegate() 生成的代码,
+与上面（当 provideDelegate 方法不存在时）生成的代码：
+
+class C {
+    var prop: Type by MyDelegate()
+}
+
+// 这段代码是当“provideDelegate”功能可用时
+// 由编译器生成的代码：
+class C {
+    // 调用“provideDelegate”来创建额外的“delegate”属性
+    private val prop$delegate = MyDelegate().provideDelegate(this, this::prop)
+    val prop: Type
+        get() = prop$delegate.getValue(this, this::prop)
+}
+ */
+
 fun main(args: Array<String>) {
     val c = Base3Impl(1000)
     Derived3(c).print()
@@ -106,4 +262,41 @@ fun main(args: Array<String>) {
 
     println(lazyValue) // 第一次执行，执行两次输出表达式
     println(lazyValue) // 第二次执行，只输出返回值
+
+    println("observable")
+    val user = User3()
+    user.name = "第一次赋值"
+    user.name = "第二次赋值"
+
+    println("map")
+    val map1 = mapOf(
+            "name" to "百度",
+            "url" to "www.baidu.com"
+    )
+    //map1.put("xx", "xx") //Map只读，没有put方法
+    val site = Site4(map1)
+    println(site.name +" " + site.url)
+    //site.name = "百度2"
+    //println(site.name +" " + site.url)
+
+    println("MutableMap")
+    var map : MutableMap<String, Any?> = mutableMapOf(
+            "name" to "Microsoft Bing",
+            "url" to "cn.bing.com"
+    )
+    val site5 = Site5(map)
+    println(site5.name +" "+site5.url)
+    map.put("name", "Google")
+    map.put("url", "www.google.com")
+    println(site5.name +" "+site5.url)
+    site5.name = "Google2"
+    //site5.url = "xx"
+    println(site5.name +" "+site5.url)
+    println(map)
+
+    println("Not Null")
+    var foo = Foo2()
+    foo.notNullBar = "bar"
+    println(foo.notNullBar)
+
 }
