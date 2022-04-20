18a3a45af3a969aa5ca7116b9b3f6d77b6544ec2
victoryckl <137456570@qq.com>
2022-04-20 11:08:01 +0800

    添加工程 	deleted:    1.txt 	new file:   bilibili/kt_base/src/com/derry/s1/KtBase07.kt 	new file:   bilibili/readme.txt

---
+++ bilibili/kt_base/src/com/derry/s1/KtBase07.kt
@@ -0,0 +1,7 @@
+package com.derry.s1
+
+fun main() {
+    println("Hello World")
+    var name: String = "Jack"
+    println(name)
+}
---
+++ bilibili/readme.txt
@@ -0,0 +1,3 @@
+【2021最新版Kotlin教程】Android Kotlin从入门到进阶全套教程（全网最完善kotlin教程）
+https://www.bilibili.com/video/BV1kT4y1o7nP
+
