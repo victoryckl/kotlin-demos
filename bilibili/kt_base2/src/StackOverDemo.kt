318a8bc1a1680ddfddf662c58cbf7d7767418516
victoryckl <137456570@qq.com>
2022-04-22 11:43:20 +0800

    换了个kotlin视频看，derry老师的看不下去了 	new file:   bilibili/kt_base2/src/StackOverDemo.kt

---
+++ bilibili/kt_base2/src/StackOverDemo.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println(add2(100, 0))
+    //ollAdd(10000) //栈溢出
+}
+
+fun ollAdd(num:Int):Int {
+    println("第${num+1}次运算")
+    if (num == 1) {
+        return 1
+    } else {
+        return num + ollAdd(num -1)
+    }
+}
+
+//尾递归优化，避免栈溢出
+tailrec fun add2(num: Int, result: Int) : Int {
+    if (num == 0) {
+        return 1
+    } else {
+        return add2(num - 1, result + num)
+    }
+}
